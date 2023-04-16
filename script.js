f10bb07cee33eb353877e12ab09f822b548f1631
adrian222 <ardipedroza@gmail.com>
2023-04-16 12:24:00 -0300

    "acá agregue el formulario"

--- script.js
+++ script.js
@@ -92,3 +92,57 @@ function vaciarCarrito() {
     }
     return false
 }
+
+//formulario
+
+const form = document.querySelector('form');
+  const nombre = document.querySelector('#nombre');
+  const email = document.querySelector('#email');
+  const telefono = document.querySelector('#telefono');
+  const servicio = document.querySelector('#servicio');
+  const cantidad = document.querySelector('#cantidad');
+  const detalles = document.querySelector('#detalles');
+  const fechaLimite = document.querySelector('#fecha_limite');
+  const presupuesto = document.querySelector('#presupuesto');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Evita que se envíe el formulario
+    
+    // Validación de campos requeridos
+    if (nombre.value === '' || email.value === '' || servicio.value === '' || cantidad.value === '' || fechaLimite.value === '') {
+      alert('Los campos marcados con * son requeridos');
+      return;
+    }
+    
+    // Validación de formato de correo electrónico
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email.value)) {
+      alert('Ingrese un correo electrónico válido');
+      return;
+    }
+
+    // Validación de formato de número de teléfono
+    const telefonoRegex = /^\d{8}$/; // asumimos que el formato de teléfono es de 8 dígitos
+if (!telefonoRegex.test(telefono.value)) {
+  alert('Ingrese un número de teléfono válido');
+  return;
+}
+
+// Envío de formulario
+const data = new FormData(form);
+fetch('/enviar_cotizacion', {
+  method: 'POST',
+  body: data
+})
+.then(response => {
+  if (response.ok) {
+    alert('La cotización ha sido enviada correctamente');
+    form.reset(); // reinicia el formulario
+  } else {
+    alert('Ocurrió un error al enviar la cotización');
+  }
+})
+.catch(error => {
+  alert('Ocurrió un error al enviar la cotización');
+  console.error(error);
+});
